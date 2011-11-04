8590cf835a1b7a2d41f74dcc6d9693d53886e5ae
trainman419 <legotown@aol.com>
2011-11-03 19:57:10 -0700

    Move ackermann_local_planner to be based on dwa_local_planner.

--- ackermann_local_planner/include/ackermann_local_planner/velocity_iterator.h
+++ ackermann_local_planner/include/ackermann_local_planner/velocity_iterator.h
@@ -2,7 +2,7 @@
 *
 * Software License Agreement (BSD License)
 *
-*  Copyright (c) 2008, Willow Garage, Inc.
+*  Copyright (c) 2009, Willow Garage, Inc.
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
@@ -15,7 +15,7 @@
 *     copyright notice, this list of conditions and the following
 *     disclaimer in the documentation and/or other materials provided
 *     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
+*   * Neither the name of Willow Garage, Inc. nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
 *
@@ -34,41 +34,52 @@
 *
 * Author: Eitan Marder-Eppstein
 *********************************************************************/
-#ifndef TRAJECTORY_ROLLOUT_WORLD_MODEL_H_
-#define TRAJECTORY_ROLLOUT_WORLD_MODEL_H_
-
-#include <vector>
-#include <costmap_2d/observation.h>
-#include <geometry_msgs/Point.h>
-#include <ackermann_local_planner/planar_laser_scan.h>
+#ifndef ACKERMANN_LOCAL_PLANNER_VELOCITY_ITERATOR_H_
+#define ACKERMANN_LOCAL_PLANNER_VELOCITY_ITERATOR_H_
+#include <algorithm>
 
 namespace ackermann_local_planner {
-  /**
-   * @class WorldModel
-   * @brief An interface the trajectory controller uses to interact with the
-   * world regardless of the underlying world model.
-   */
-  class WorldModel{
+  class VelocityIterator {
     public:
-      /**
-       * @brief  Subclass will implement this method to check a footprint at a given position and orientation for legality in the world
-       * @param  position The position of the robot in world coordinates
-       * @param  footprint The specification of the footprint of the robot in world coordinates
-       * @param  inscribed_radius The radius of the inscribed circle of the robot
-       * @param  circumscribed_radius The radius of the circumscribed circle of the robot
-       * @return Positive if all the points lie outside the footprint, negative otherwise
-       */
-      virtual double footprintCost(const geometry_msgs::Point& position, const std::vector<geometry_msgs::Point>& footprint,
-          double inscribed_radius, double circumscribed_radius) = 0;
+      VelocityIterator(double min, double max, double step_size):
+        min_(min),
+        max_(max),
+        step_size_(step_size),
+        current_sample_(min),
+        finished_(false)
+      {
+      }
 
-      /**
-       * @brief  Subclass will implement a destructor
-       */
-      virtual ~WorldModel(){}
+      double getVelocity(){
+        return current_sample_;
+      }
 
-    protected:
-      WorldModel(){}
+      VelocityIterator& operator++(int){
+        if(current_sample_ == max_){
+          finished_ = true;
+          return *this;
+        }
+
+        double next_sample_ = current_sample_ + step_size_;
+
+        if(next_sample_ * current_sample_ < 0.0)
+          current_sample_ = 0.0;
+        else
+          current_sample_ = next_sample_;
+
+        if(current_sample_ >= max_)
+          current_sample_ = max_;
+
+        return *this;
+      }
+
+      bool isFinished(){
+        return finished_;
+      }
+
+    private:
+      double min_, max_, step_size_, current_sample_;
+      bool finished_;
   };
-
 };
 #endif
