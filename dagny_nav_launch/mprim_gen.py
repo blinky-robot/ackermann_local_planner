36acec6df2e36aafd4ba5cc7fc82a4813338c631
trainman419 <legotown@aol.com>
2013-11-13 22:50:47 -0800

    More work on primitive generation.

--- dagny_nav_launch/mprim_gen.py
+++ dagny_nav_launch/mprim_gen.py
@@ -1,27 +1,69 @@
 #!/usr/bin/env python
 
+import sys
 import mprim
+
+# mirror about the X axis
+def mirror_x(p):
+    return (p[0], -p[1], -p[2])
+
+# mirror about the Y axis
+def mirror_y(p):
+    return (-p[0], p[1], -p[2])
+
+# mirror about x=y
+def mirror_xy(p):
+    return (p[1], p[0], -p[2])
+
+# mirror about x=-y
+def mirror_x_y(p):
+    return (-p[1], -p[0], p[2])
 
 def expand_primitives(prim):
     # mirror angle 0 primitives about X
     prim_0 = list(prim[0])
     for p in prim[0]:
         if p[2] != 0:
-            prim_0.append((p[0], -p[1], -p[2]))
+            prim_0.append(mirror_x(p))
     prim[0] = prim_0
     # mirror angle 2 primitives about x=y
     prim_2 = list(prim[2])
     for p in prim[2]:
         if p[2] != 0:
-            prim_2.append((p[1], p[0], -p[2]))
+            prim_2.append(mirror_xy(p))
     prim[2] = prim_2
     # mirror angle 1 primitives about x=y
     prim[3] = []
     for p in prim[1]:
-        prim[3].append((p[1], p[0], -p[2]))
+        prim[3].append(mirror_xy(p))
 
+    # rotate and mirror primitives about the origin
+    for i in range(4):
+        # +4 mirror about Y
+        prim[i+4] = map(mirror_y, prim[i])
+        # +8 mirror about x=-y
+        prim[i+8] = map(mirror_x_y, prim[i])
+        # +12 mirror about X
+        prim[i+12] = map(mirror_x, prim[i])
+
+def generate_mprim(prim):
+    res = {}
+    for start_a in prim:
+        start = (0, 0, start_a)
+        res[start_a] = []
+        for end_a in prim[start_a]:
+            end_b = (end_a[0], end_a[1], start_a + end_a[2])
+            poses = [ start, end_b ]
+            res[start_a].append(mprim.MPrim(start, end_b, poses))
+    return res
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser('Motion primitive generation')
+    parser.add_argument('-o', '--output', help="Output file")
+
+    args = parser.parse_args()
+
     # primitives should explicitly allow differing numbers of primitives
     # per start angle
     # we should also have reflection/rotation functions to generate the
@@ -34,7 +76,13 @@ def main():
 
     expand_primitives(primitives)
 
-    print primitives
+    prim = generate_mprim(primitives)
+
+    if not args.output:
+        import yaml
+        print yaml.dump(mprim)
+    else:
+        mprim.write_mprim(args.output, prim, 0.1)
 
 if __name__ == '__main__':
     main()
