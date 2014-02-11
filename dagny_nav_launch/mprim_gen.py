b7bb7c23cf2f86fc5b318578595a24a948024a45
trainman419 <legotown@aol.com>
2014-02-11 08:58:36 -0800

    Implement both methods of snapping angles to grid

--- dagny_nav_launch/mprim_gen.py
+++ dagny_nav_launch/mprim_gen.py
@@ -12,7 +12,7 @@ from primitives import *
 from pylab import *
 
 def normalize(angle, max_angle):
-    while angle > max_angle:
+    while angle >= max_angle:
         angle = angle - max_angle 
     while angle < 0:
         angle = angle + max_angle
@@ -21,8 +21,11 @@ def normalize(angle, max_angle):
 def index_angle(angle, num_angles):
     print angle / math.pi
     # Assuming angles are evenly distributed
-    num = round( angle * num_angles / ( math.pi * 2 ))
-    print num
+    n1 = round( angle * num_angles / ( math.pi * 2 ))
+    n1 = normalize(n1, num_angles)
+    print n1
+    #r1 = n1 * math.pi * 2 / num_angles
+    #print r1
     # Assuming angles snap to the nearest endpoint
     #  assume num_angles = 16
     s = math.sin(angle)
@@ -30,33 +33,100 @@ def index_angle(angle, num_angles):
     if abs(s) > abs(c):
         # normalize sine(y) to 1
         if s == 0:
+            # We should never hit this if abs(s) > abs(c)
+            assert(False)
             norm = 1.0
         else:
             norm = abs(1.0 / s)
+        plus = round(norm * c * (num_angles / 8))
+        x = plus / (num_angles / 8)
+        # upper or lower triangle
+        #  base 1(upper) or 3(lower)
+        if s > 0:
+            y = 1
+            base = 1
+            plus = -plus
+        elif s < 0:
+            y = -1
+            base = 3
+        else:
+            # ??
+            assert(False)
+            y = 0
+    else:
+        # normalize cos(x) to 1
+        if c == 0:
+            # ??. we should never get here, but we do?
+            assert(False)
+            norm = 1.0
+        else:
+            norm = abs(1.0 / c)
+        plus = round(norm * s * (num_angles / 8))
+        y = plus / (num_angles / 8)
+        # left or right triangle
+        #  base 0(right) or 2(left)
+        if c > 0:
+            x = 1
+            base = 0
+        elif c < 0:
+            x = -1
+            base = 2
+            plus = -plus
+        else:
+            # ?? 
+            assert(False)
+            x = 0
+    base = base * (num_angles / 4)
+    #r2 = math.atan2(y, x)
+    #print r2
+    n2 = normalize(base + plus, num_angles)
+    print n2
+    print
+
+def round_angle(angle, num_angles):
+    print angle / math.pi
+    # Assuming angles are evenly distributed
+    n1 = round( angle * num_angles / ( math.pi * 2 ))
+    n1 = normalize(n1, num_angles)
+    r1 = normalize(n1 * math.pi * 2 / num_angles, math.pi * 2)
+    print r1
+    # Assuming angles snap to the nearest endpoint
+    #  assume num_angles = 16
+    s = math.sin(angle)
+    c = math.cos(angle)
+    if abs(s) > abs(c):
+        # normalize sine(y) to 1
+        if s == 0:
+            assert(False)
+        else:
+            norm = abs(1.0 / s)
         x = round(norm * c * (num_angles / 8)) / (num_angles / 8)
+        # upper or lower triangle
         if s > 0:
             y = 1
         elif s < 0:
             y = -1
         else:
-            y = 0
+            # ??
+            assert(False)
     else:
         # normalize cos(x) to 1
         if c == 0:
-            norm = 1.0
+            assert(False)
         else:
             norm = abs(1.0 / c)
         y = round(norm * s * (num_angles / 8)) / (num_angles / 8)
+        # left or right triangle
         if c > 0:
             x = 1
         elif c < 0:
             x = -1
         else:
-            x = 0
-    print x, y
+            # ?? 
+            assert(False)
+    r2 = normalize(math.atan2(y, x), math.pi * 2)
+    print r2
     print
-
-def round_angle(angle, num_angles):
     pass
 
 # mirror about the X axis
@@ -424,4 +494,5 @@ if __name__ == '__main__':
     # simple test for index_angle
     #for angle in numpy.arange(0, math.pi*2, math.pi / 64 ):
     #    index_angle(angle, 16)
+    #    round_angle(angle, 16)
     main()
